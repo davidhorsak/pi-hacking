c97583a48086ed9f47d4dde97c4427e25c872d47
David Horsak <david.horsak@about.me>
2016-03-23 22:12:00 +0100

    full 180 move

--- motor.py
+++ motor.py
@@ -6,12 +6,31 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 
 
-width = 0.0015
+break_width = 0.002
+middle_width = 0.00152
+deviation    = 0.0012
+
+min_width = middle_width - deviation
+max_width = middle_width + deviation
+
+def length(angle):
+    return min_width + (angle/180.0) * (max_width - min_width)
 
 while(1):
-    GPIO.output(17, 1)
-    sleep(width)
-    GPIO.output(17, 0)
-    sleep(width)
+   for angle in range(0, 180):
+        GPIO.output(17, 1)
+        l = length(angle)
+        print l
+        sleep(l)
+        GPIO.output(17, 0)
+        sleep(break_width)
+
+   for angle in range(0, 180):
+        GPIO.output(17, 1)
+        l = length(180 - angle)
+        print l
+        sleep(l)
+        GPIO.output(17, 0)
+        sleep(break_width)
 
 GPIO.cleanup()
